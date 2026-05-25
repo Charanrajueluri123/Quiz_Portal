71d2368a26fee9ec88a34281e2cd9e5cccd135fa
Charanrajueluri123 <2100031448cseh@gmail.com>
2026-05-25 10:51:38 +0530

    Docker SetUp Done

--- src/main/java/com/quiz/util/DBConnection.java
+++ src/main/java/com/quiz/util/DBConnection.java
@@ -1,42 +1,37 @@
 package com.quiz.util;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
 public class DBConnection {
-	private static final String PROPERTIES_FILE = "/db.properties";
 
-	private static Properties loadProperties() {
-		Properties props = new Properties();
-		try (InputStream in = DBConnection.class.getResourceAsStream(PROPERTIES_FILE)) {
-			if (in != null) {
-				props.load(in);
-			} else {
-				System.err.println("db.properties not found on classpath, falling back to defaults");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return props;
-	}
+//    private static final String URL = "jdbc:mysql://localhost:3306/quiz_db";
+//    private static final String USER = "root";
+//    private static final String PASSWORD = "2004ecru";
+//
+//    public static Connection getConnection() {
+//        Connection con = null;
+//
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            con = DriverManager.getConnection(URL, USER, PASSWORD);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        return con;
+//    }
 
+	private static Connection con;
 	public static Connection getConnection() {
-		Connection con = null;
-		Properties props = loadProperties();
-		String url = props.getProperty("db.url", "jdbc:mysql://localhost:3306/quiz_db?useSSL=false&serverTimezone=UTC");
-		String user = props.getProperty("db.user", "root");
-		String pass = props.getProperty("db.password", "");
-
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(url, user, pass);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://dpg-d89tfnfavr4c73cv1evg-a.ohio-postgres.render.com:5432/quizdb_tm3u";
+			String username = "quizuser";
+			String password = "OGmLiFn16kuhE7WGTBUtbxo2NfCiCuv2";
+			con = DriverManager.getConnection(url, username, password);
+			System.out.println("Database Connected Successfully!");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return con;
