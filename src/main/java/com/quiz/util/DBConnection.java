960747b597310d461d55ec193ee6fc1b094f7e20
Charanrajueluri123 <2100031448cseh@gmail.com>
2026-06-28 11:52:23 +0530

    newly created db in render

--- src/main/java/com/quiz/util/DBConnection.java
+++ src/main/java/com/quiz/util/DBConnection.java
@@ -23,19 +23,36 @@ public class DBConnection {
 //    }
 
 	private static Connection con;
-	public static Connection getConnection() {
-		try {
-			Class.forName("org.postgresql.Driver");
-			String url = "jdbc:postgresql://dpg-d89tfnfavr4c73cv1evg-a.ohio-postgres.render.com:5432/quizdb_tm3u";
-			String username = "quizuser";
-			String password = "OGmLiFn16kuhE7WGTBUtbxo2NfCiCuv2";
-			con = DriverManager.getConnection(url, username, password);
-			System.out.println("Database Connected Successfully!");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return con;
-	}
+//	public static Connection getConnection() {
+//		try {
+//			Class.forName("org.postgresql.Driver");
+//			String url = "jdbc:postgresql://dpg-d89tfnfavr4c73cv1evg-a.ohio-postgres.render.com:5432/quizdb_tm3u";
+//			String username = "quizuser";
+//			String password = "OGmLiFn16kuhE7WGTBUtbxo2NfCiCuv2";
+//			con = DriverManager.getConnection(url, username, password);
+//			System.out.println("Database Connected Successfully!");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return con;
+//	}
+	
+	    public static Connection getConnection() {
+	        try {
+	            Class.forName("org.postgresql.Driver");
+
+	            String url = "jdbc:postgresql://dpg-d90bfikm0tmc73dl1b40-a.ohio-postgres.render.com:5432/quizdb1_itkv";
+	            String username = "quizdb1_itkv_user";
+	            String password = "b6uni27KTr2b1o711OPO4B9iokUYu9Qr";
+
+	            con = DriverManager.getConnection(url, username, password);
+	            System.out.println("Database Connected Successfully!");
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return con;
+	    }
+	
 	
 	
 }
